5e6383a672e18ec5506b7c1514e9acb1efcaaa91
Jonatan Flores <ti02@alfasistemas.inf.br>
2020-04-16 21:44:05 -0300

    added global exception handler

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const BaseExceptionHandler = use("BaseExceptionHandler");
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle(error, { request, response }) {
+    // Change the errors response
+    if (error.name === "ValidationException") {
+      const errors = {};
+
+      error.messages.forEach((error) => {
+        if (!errors[error.field]) {
+          errors[error.field] = error.message;
+        }
+      });
+
+      return response.status(error.status).send(errors);
+    }
+
+    return super.handle(...arguments);
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report(error, { request }) {}
+}
+
+module.exports = ExceptionHandler;
